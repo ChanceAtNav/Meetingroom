eb86f623a541b31e4a60f11ee57f7ed66c549f3e
Chance Marrott <chance@nav.com>
2017-04-26 09:15:52 -0600

    First Commit

---
+++ pages/clickscript.js
@@ -0,0 +1,8 @@
+var classname = document.getElementsByClassName("c-time")
+
+var myfunc = function() {
+    console.log("working")
+}
+
+document.getElementsByClassName("c-time").addEventListener('click', myfunc)
+
